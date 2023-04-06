cfaba5df1f4b598af76abe7e00211299025e30e9
ian balijawa <ianbalijawa16@gmail.com>
2023-04-06 01:20:33 -0700

    refactor

--- mail.js
+++ mail.js
@@ -2,6 +2,11 @@
 require("dotenv/config");
 const nodemailer = require("nodemailer");
 
+const to = `${process.env.RECIPIENT_EMAIL},'',''`;
+const bcc = ",,";
+const cc = ",,";
+const attachments = [];
+
 async function main() {
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -15,10 +20,13 @@ async function main() {
 
   let info = await transporter.sendMail({
     from: `"GHOST 👻" ${process.env.SENDER_EMAIL}`,
-    to: process.env.RECIPIENT_EMAIL,
+    to,
     subject: "Hello ✔",
     text: "Hello world?",
     html: "<h1>Hello world?</h1>",
+    bcc,
+    cc,
+    attachments,
   });
 
   console.log("Message sent: %s", info.messageId);
